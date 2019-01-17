b2def7e99335f36f12220dc05fb014b813423ff9
hpamnani <harshpam1993@gmail.com>
2019-01-17 19:35:29 -0400

    my hello world application

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean clicked;
+    private Button colorChanger;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        colorChanger = findViewById(R.id.btnChangeText);
+        clicked=false;
+
+        colorChanger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!clicked) {
+                    colorChanger.setText(getResources().getText(R.string.btn_clicked));
+                    colorChanger.setBackgroundColor(getResources().getColor(R.color.colorRed,getResources().newTheme()));
+                    clicked=true;
+                } else {
+                    colorChanger.setText(getResources().getText(R.string.btn_not_clicked));
+                    colorChanger.setBackgroundColor(getResources().getColor(R.color.colorGray,getResources().newTheme()));
+                    clicked=false;
+                }
+            }
+        });
+    }
+}
